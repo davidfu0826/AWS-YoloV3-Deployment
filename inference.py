9330be3f24e471ea540f1f2521578e06d941099f
davidfu0826 <david.bao.fu@gmail.com>
2022-02-14 16:43:22 +0100

    [add] Inference test script

---
+++ inference.py
@@ -0,0 +1,40 @@
+import subprocess
+import glob
+
+def run_prediction(path_to_dir_or_img):
+    #subprocess.run(["python", "--version"])
+    #subprocess.run(["python", "yolov3/detect.py", "--help"])
+    subprocess.run([
+        "python", "yolov3/detect.py",
+        "--source", path_to_dir_or_img, 
+        "--project", "output", "--name", "results", "--exist-ok", # Inference results saved to "output/results"
+        "--device", "cpu",
+        "--save-txt",
+        "--save-conf"
+        ])
+
+def read_detection_results(output_txts):
+    """Assuming single image inference
+    """
+    txt_paths = glob.glob(output_txts)
+
+    for txt_path in txt_paths:
+        with open(txt_path, 'r') as f:
+            lines = [line.replace('\n', '').split() for line in f.readlines()]
+        for line in lines:
+            class_idx = int(line[0])
+            loc_x = float(line[1])
+            loc_y = float(line[2])
+            box_w = float(line[3])
+            box_h = float(line[4])
+            conf = float(line[5])
+        print(lines)
+    return output
+
+if __name__ == "__main__":
+    path_to_dir_or_img = "C:/Users/Succe/OneDrive/Photo, Video and music/Pictures/240740560_156617623198297_7836862078333369339_n.jpg"
+    output_img = "output/results/*.jpg"
+    output_txts = "output/results/labels/*.txt"
+
+    #run_prediction(path_to_dir_or_img)
+    read_detection_results(output_txts)
