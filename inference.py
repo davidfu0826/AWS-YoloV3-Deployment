cce21eb02eeeebc347c5fdd3f425519afa417ebb
davidfu0826 <david.bao.fu@gmail.com>
2022-02-15 00:22:54 +0100

    [add] Docker Compose

--- inference.py
+++ inference.py
@@ -1,5 +1,6 @@
 import subprocess
 import glob
+import shutil
 
 def run_prediction(path_to_dir_or_img):
     #subprocess.run(["python", "--version"])
@@ -18,23 +19,39 @@ def read_detection_results(output_txts):
     """
     txt_paths = glob.glob(output_txts)
 
+    results = []
     for txt_path in txt_paths:
         with open(txt_path, 'r') as f:
             lines = [line.replace('\n', '').split() for line in f.readlines()]
-        for line in lines:
+        for idx, line in enumerate(lines):
             class_idx = int(line[0])
             loc_x = float(line[1])
             loc_y = float(line[2])
             box_w = float(line[3])
             box_h = float(line[4])
             conf = float(line[5])
-        print(lines)
-    return output
+
+            bbox = {
+                "class_idx": class_idx,
+                "loc_x": float(line[1]),
+                "loc_y": float(line[2]),
+                "box_w": float(line[3]),
+                "box_h": float(line[4]),
+                "conf": float(line[5])
+            }
+            results.append(bbox)
+
+    #print(results)
+    return results
 
 if __name__ == "__main__":
+    
     path_to_dir_or_img = "C:/Users/Succe/OneDrive/Photo, Video and music/Pictures/240740560_156617623198297_7836862078333369339_n.jpg"
-    output_img = "output/results/*.jpg"
+    #output_img = "output/results/*.jpg"
     output_txts = "output/results/labels/*.txt"
 
-    #run_prediction(path_to_dir_or_img)
-    read_detection_results(output_txts)
+    run_prediction(path_to_dir_or_img)
+    results = read_detection_results(output_txts)
+
+    # Clean
+    shutil.rmtree("output/results/")
