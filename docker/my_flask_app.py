68c42c37313e0789be818e13f25423dff9883147
Fu_David <david.bao.fu@sony.com>
2022-04-22 09:48:37 +0200

    [add] Redoing work

--- docker/my_flask_app.py
+++ docker/my_flask_app.py
@@ -1,19 +1,20 @@
+from genericpath import exists
+import string
+import random
+from pathlib import Path
 import pickle
 import numpy as np
 from flask import Flask, request
 from PIL import Image
 
-#from predict import run_prediction, read_detection_results
+from predict import run_prediction, read_detection_results
 
 model = None
 app = Flask(__name__)
 
-"""
-def load_model():
-    global model
-    with open('iris_trained_model.pkl', 'rb') as f:
-        model = pickle.load(f)
-"""
+def random_string(length):
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(length))
 
 @app.route('/')
 def home_endpoint():
@@ -25,25 +26,24 @@ def ping_response():
 
 @app.route('/invocations', methods=['POST'])
 def get_prediction():
-    """
+
     if request.method == 'POST':
         print(request.files)
         #data = request.get_json()  # Get data posted as a json
         #data = np.array(data)[np.newaxis, :]  # converts shape from (4,) to (1, 4)
-        image=request.files['media']
+        image=request.files['image']
         print(image)
+        Path("data/input/").mkdir(exist_ok=True, parents=True)
         pil_image = Image.open(image)
         print(type(pil_image), pil_image)
-        pil_image.save("data/input/image.jpg")
 
-        run_prediction("data/input/image.jpg")
-        response = read_detection_results("data/input/image.jpg", "output/results/labels/*.txt", "yolov3/data/coco128.yaml")
-        #prediction = model.predict(data)  # runs globally loaded model on the data
+        filename = random_string(20)
+        pil_image.save(f"data/input/{filename}.jpg")
+
+        run_prediction(f"data/input/{filename}.jpg")
+        response = read_detection_results(f"data/input/{filename}.jpg", f"data/output/{filename}/labels/*.txt", "yolov3/data/coco128.yaml")
     return str(response)
-    """
-    return str({
-        "results": [1,2,3]
-    })
+
 
 #if __name__ == '__main__':
 #    #load_model()  # load model at the beginning once only
